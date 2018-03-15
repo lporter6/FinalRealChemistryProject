6d82c5e377423e3e744eb64f75d934ec388b29c9
Cole Petrocci <bling435@gmail.com>
2018-03-15 14:16:59 -0400

    CHANGED A LOT OF SHIT: Added new listners into MainFrameActionListener, messed with the buttons class

--- src/Main/MainFrameActionListener.java
+++ src/Main/MainFrameActionListener.java
@@ -1,14 +1,19 @@
 package Main;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import MolarMass.MolarMass;
 import GasLaws.GasLaws;
-import GasLaws.Equations;
+import GasLaws.Buttons;
 
 public class MainFrameActionListener implements ActionListener{
-	
+
+	public static GasLaws G = new GasLaws();
+
+	public static Buttons B = new Buttons();
+
 	public void actionPerformed(ActionEvent e){
 			
 		if("MolarMass".equals(e.getActionCommand())){ //gets called if the button that is pressed has an Action Command value of "MolarMass"
@@ -17,6 +22,10 @@ public class MainFrameActionListener implements ActionListener{
 			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
 			Start.frame.setContentPane(MolarMass.molarMassPanel); //sets the content pane to the molar mass panel
 			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			Start.c.gridx = 2;
+			Start.c.gridy = 9;
+			MolarMass.molarMassPanel.add(Start.homeButton, Start.c);
 			
 		}else if("Molar Enter".equals(e.getActionCommand())){ //gets called if the button that is pressed has a action command of "Molar Enter"
 			
@@ -48,23 +57,141 @@ public class MainFrameActionListener implements ActionListener{
 			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
 			Start.frame.setContentPane(GasLaws.gasLawsPanel); //sets the content pane to the molar mass panel
 			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
-			
+
+			//Home button on main gasLawsPanel
+			G.bag.gridx = 4;
+			G.bag.gridy = 0;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.gasLawsPanel.add(Start.homeButton, G.bag);
+
+
 		}else if("Gas Enter".equals(e.getActionCommand())){
 
 		
-		}else if("Volume Enter".equals(e.getActionCommand())){
-			
+		}else if("Volume Enter".equals(e.getActionCommand())) {
+
 			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
 			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
-			Start.frame.setContentPane(GasLaws.volumePanel); //sets the content pane to the molar mass panel
+			Start.frame.setContentPane(GasLaws.volumePanel); //sets the content pane to the volume panel
 			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
-			
+
+			//Volume home button
+			G.bag.gridx = 7;
+			G.bag.gridy = 0;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.volumePanel.add(Start.homeButton, G.bag);
+
+		}else if("Volume PV".equals(e.getActionCommand())) {
+
+			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
+			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
+			Start.frame.setContentPane(GasLaws.volumePV_Panel); //sets the content pane to the molar mass panel
+			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Adds enter button to volumePV_Panel
+			GasLaws.bag.gridx = 0;
+			GasLaws.bag.gridy = 1;
+			Buttons.enterButtonGasLaws.setActionCommand("Gas Enter");
+			Buttons.enterButtonGasLaws.addActionListener(Start.al1);
+			G.volumePV_Panel.add(Buttons.enterButtonGasLaws, G.bag);
+
+			G.bag.gridx = 7;
+			G.bag.gridy = 1;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.volumePV_Panel.add(Start.homeButton, G.bag);
+		}else if("Volume PVT".equals(e.getActionCommand())) {
+
+			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
+			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
+			Start.frame.setContentPane(GasLaws.volumePVT_Panel); //sets the content pane to the molar mass panel
+			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Adds enter button to volumePVT_Panel
+			G.bag.gridx = 0;
+			G.bag.gridy = 1;
+			Buttons.enterButtonGasLaws.setActionCommand("Gas Enter");
+			Buttons.enterButtonGasLaws.addActionListener(Start.al1);
+			G.volumePVT_Panel.add(Buttons.enterButtonGasLaws, G.bag);
+
+			G.bag.gridx = 7;
+			G.bag.gridy = 1;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.volumePVT_Panel.add(Start.homeButton, G.bag);
+		}else if("Volume VT".equals(e.getActionCommand())) {
+
+			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
+			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
+			Start.frame.setContentPane(GasLaws.volumeVT_Panel); //sets the content pane to the molar mass panel
+			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Adds enter button to volumeVT_Panel
+			G.bag.gridx = 0;
+			G.bag.gridy = 1;
+			Buttons.enterButtonGasLaws.setActionCommand("Gas Enter");
+			Buttons.enterButtonGasLaws.addActionListener(Start.al1);
+			G.volumeVT_Panel.add(Buttons.enterButtonGasLaws, G.bag);
+
+			G.bag.gridx = 7;
+			G.bag.gridy = 1;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.volumeVT_Panel.add(Start.homeButton, G.bag);
+		}else if("Volume PVnRT".equals(e.getActionCommand())) {
+
+			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
+			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
+			Start.frame.setContentPane(GasLaws.volumePVnRT_Panel); //sets the content pane to the molar mass panel
+			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Adds enter button to volumePVnRT_Panel
+			G.bag.gridx = 0;
+			G.bag.gridy = 1;
+			Buttons.enterButtonGasLaws.setActionCommand("Gas Enter");
+			Buttons.enterButtonGasLaws.addActionListener(Start.al1);
+			G.volumePVnRT_Panel.add(Buttons.enterButtonGasLaws, G.bag);
+
+			G.bag.gridx = 7;
+			G.bag.gridy = 1;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.volumePVnRT_Panel.add(Start.homeButton, G.bag);
+		}else if("Volume VN".equals(e.getActionCommand())) {
+
+			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
+			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
+			Start.frame.setContentPane(GasLaws.volumeVN_Panel); //sets the content pane to the molar mass panel
+			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Adds enter button to volumeVN_Panel
+			G.bag.gridx = 0;
+			G.bag.gridy = 1;
+			Buttons.enterButtonGasLaws.setActionCommand("Gas Enter");
+			Buttons.enterButtonGasLaws.addActionListener(Start.al1);
+			G.volumeVN_Panel.add(Buttons.enterButtonGasLaws, G.bag);
+
+			G.bag.gridx = 7;
+			G.bag.gridy = 1;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.volumeVN_Panel.add(Start.homeButton, G.bag);
+
 		}else if("Pressure Enter".equals(e.getActionCommand())){
 			
 			Start.frame.getContentPane().setVisible(false); //gets the frames current content pane and sets its visibility to false
 			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
 			Start.frame.setContentPane(GasLaws.pressurePanel); //sets the content pane to the molar mass panel
 			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Pressure home buttons
+			G.bag.gridx = 4;
+			G.bag.gridy = 0;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.pressurePanel.add(Start.homeButton, G.bag);
 			
 		}else if("Moles Enter".equals(e.getActionCommand())){
 			
@@ -72,6 +199,13 @@ public class MainFrameActionListener implements ActionListener{
 			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
 			Start.frame.setContentPane(GasLaws.molesPanel); //sets the content pane to the molar mass panel
 			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Moles home buttons
+			G.bag.gridx = 4;
+			G.bag.gridy = 0;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.molesPanel.add(Start.homeButton, G.bag);
 			
 		}else if("Temp Enter".equals(e.getActionCommand())){
 			
@@ -79,6 +213,13 @@ public class MainFrameActionListener implements ActionListener{
 			Start.frame.remove(Start.frame.getContentPane()); //removes the current content pane from the frame
 			Start.frame.setContentPane(GasLaws.tempPanel); //sets the content pane to the molar mass panel
 			Start.frame.getContentPane().setVisible(true); //gets the current content pane and sets its visibility to true to ensure visibility
+
+			//Temperature home buttons
+			G.bag.gridx = 4;
+			G.bag.gridy = 0;
+			Start.homeButton.setActionCommand("Home");
+			Start.homeButton.addActionListener(Start.al1);
+			G.tempPanel.add(Start.homeButton, G.bag);
 
 		}else if("Game".equals(e.getActionCommand())){
 			
