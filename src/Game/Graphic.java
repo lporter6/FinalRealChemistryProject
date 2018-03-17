147348361f9d03fa35b759e49a4bf4bf5a70825e
lporter6 <porterlogan6@gmail.com>
2018-03-16 22:23:32 -0400

    Edited game controls

--- src/Game/Graphic.java
+++ src/Game/Graphic.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 public class Graphic extends JPanel{
 	
 	public Graphics2D g2d;
+
 	
 	
 	private static final long serialVersionUID = 1L;
@@ -15,6 +16,7 @@ public class Graphic extends JPanel{
 		
 		g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //adds smooth moving
+		g2d.drawImage(Build.background, -1366, -768, Build.frame.getWidth()*2, Build.frame.getHeight()*2, this);
 		g2d.drawImage(Build.character.sprite, Build.character.x, Build.character.y, Build.character.width, Build.character.height,this);
 	}
 	
