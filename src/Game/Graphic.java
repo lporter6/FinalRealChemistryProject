03f62cee1019a8c5ae116c1bb1a72be079d21d77
lporter6 <porterlogan6@gmail.com>
2018-03-22 09:52:55 -0400

    added new components to graphic

--- src/Game/Graphic.java
+++ src/Game/Graphic.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 public class Graphic extends JPanel{
 	
 	public Graphics2D g2d;
-	public TileMap map = new TileMap(40,80);
+
 	
 	
 	private static final long serialVersionUID = 1L;
@@ -18,7 +18,7 @@ public class Graphic extends JPanel{
 		
 		g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //adds smooth moving
-		map.drawTilePiece(g2d);
+		Build.map.drawTilePiece(g2d);
 		g2d.drawImage(Build.character.getSprite(), Build.character.getX(), Build.character.getY(), Build.character.getWidth(), Build.character.getHeight(),this);
 	}
 	
